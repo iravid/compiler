57df9143af6e7598df70af908ea78a4479336d73
Itamar Ravid <iravid@iravid.com>
2015-01-19 21:07:17 +0200

    Finished transcribing the grammar production rules.

--- src/parser.py
+++ src/parser.py
@@ -14,10 +14,15 @@ def p_declarations(p):
 def p_declarelist(p):
     """declarelist : declarelist type COLON idents
                     | type COLON idents
-                    | declarelist ctype ID CONSTASSIGN INTEGER SEMICOLON
-                    | declarelist ctype ID CONSTASSIGN FLOAT SEMICOLON
+                    | declarelist ctype ID CONSTASSIGN number SEMICOLON
+                    | declarelist ctype ID CONSTASSIGN number SEMICOLON
                     | ctype ID CONSTASSIGN INTEGER SEMICOLON
                     | ctype ID CONSTASSIGN FLOAT SEMICOLON"""
+    pass
+
+def p_number(p):
+    """number : INTEGER
+              | FLOAT"""
     pass
 
 def p_idents(p):
@@ -57,4 +62,70 @@ def p_read_stmt(p):
 
 def p_assignment_stmt(p):
     "assignment_stmt : ID ASSIGN expression SEMICOLON"
+    pass
+
+def p_val(p):
+    """val : ID ASSIGN IVAL LPAREN expression RPAREN SEMICOLON
+           | ID ASSIGN RVAL LPAREN expression RPAREN SEMICOLON"""
+    pass
+
+def p_control_stmt(p):
+    """control_stmt : IF LPAREN boolexpr RPAREN THEN stmt OTHERWISE stmt
+                    | WHILE LPAREN boolexpr RPAREN DO stmt
+                    | FROM assignment_stmt TO boolexpr WHEN step DO stmt"""
+    pass
+
+def p_step(p):
+    """step : ID ASSIGN ID addop number
+            | ID ASSIGN ID mulop number"""
+    pass
+
+def p_addop(p):
+    """addop : PLUS
+             | MINUS"""
+    pass
+
+def p_mulop(p):
+    """mulop : MULT
+             | DIV"""
+    pass
+
+def p_boolexpr(p):
+    """boolexpr : boolexpr OR boolterm
+                | boolterm"""
+    pass
+
+def p_boolterm(p):
+    """boolterm : boolterm AND boolfactor
+                | boolfactor"""
+    pass
+
+def p_boolfactor(p):
+    """boolfactor : EXCLAMATION LPAREN boolfactor RPAREN
+                  | expression relop expression"""
+    pass
+
+def p_relop(p):
+    """relop : EQ
+             | NEQ
+             | LT
+             | LTE
+             | GT
+             | GTE"""
+    pass
+
+def p_expression(p):
+    """expression : expression addop term
+                  | term"""
+    pass
+
+def p_term(p):
+    """term : term mulop factor
+            | factor"""
+    pass
+
+def p_factor(p):
+    """factor : LPAREN expression RPAREN
+              | ID
+              | number"""
     pass
